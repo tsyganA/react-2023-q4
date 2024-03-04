7fb964f758800a2c22ff8a4aaf9197a9422b6e21
Anton Tsyhan <anton.tsyhan@uco.co.il>
2024-03-05 01:38:48 +0300

    new branch add

--- vite-project/src/api/requests-types.ts
+++ vite-project/src/api/requests-types.ts
@@ -18,20 +18,7 @@ export interface ShortPersonRequest {
   eye_color: string;
   hair_color: string;
   url: string;
-  // id: string;
-  // baseImageUrl: string;
 }
-
-// export interface TransformPerson {
-//   id: string;
-//   name: string;
-//   birthYear: string;
-//   gender: string;
-//   eyeColor: string;
-//   height: string;
-//   hair_color: string;
-//   img: string;
-// }
 
 export interface iErrorBoundaryProps {
   children: React.ReactNode;
@@ -40,26 +27,3 @@ export interface iErrorBoundaryProps {
 export interface iRenderRequest {
   renderRequest: ShortPersonRequest[];
 }
-
-// export interface ResponseApi {
-//   count: number;
-//   next: string | null;
-//   previous: string | null;
-//   results: ShortPersonRequest[];
-// }
-
-// export interface AppStateToProps {
-//   mainState: AppState;
-// }
-
-// export interface AppState {
-//   term: string;
-//   people: TransformPerson[];
-//   setSearchTerm: (newTerm: string) => void;
-//   searchPerson: () => void;
-//   loading: boolean;
-// }
-
-// export interface EmptyProps {}
-
-// export interface EmptyState {}
